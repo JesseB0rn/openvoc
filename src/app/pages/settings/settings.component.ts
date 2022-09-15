bec1374a4dc95819a9c830ec482b6e91684f8803
Jesse Born <jesse.born@gmx.ch>
2022-09-15 13:41:05 +0200

    added a sign out button

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { SignOut } from 'src/app/actions/auth.actions';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  constructor(private store: Store) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signOut() {
+    this.store.dispatch(new SignOut());
   }
-
 }
--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { ToolbarModule } from 'src/app/components/toolbar/toolbar.module';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 
 @NgModule({
   declarations: [SettingsComponent],
-  imports: [CommonModule, ToolbarModule],
+  imports: [CommonModule, ToolbarModule, MaterialModule],
 })
 export class SettingsModule {}
