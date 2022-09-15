8a96f5a7df9195699e5e3ca4f8d67b9bcf6c0208
Jesse Born <jesse.born@gmx.ch>
2022-09-15 21:46:49 +0200

    auth guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,33 @@ import { LearnComponent } from './pages/learn/learn.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import {
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+  AuthGuard,
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
+
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'settings',
     component: SettingsComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'create',
     component: CreateComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'landing',
@@ -29,10 +44,14 @@ const routes: Routes = [
   {
     path: 'learn',
     component: LearnComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
   },
   {
     path: 'register',
